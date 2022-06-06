03a38a24b7f7c30df168728127de76c5eeacb51c
1KVN0 <106463009+1KVN0@users.noreply.github.com>
2022-06-06 04:36:45 -1000

    Fixes and stuff
    - Updated Agora's icon in credits
    - Added links to Neyecole and TIP's social media for credits
    - Fixed Dialogue portraits glitching out for some reason lol
    - Added Discord RPC
    - Fixed some little clipping errors (Pomu on the intro and the story mode and option buttons

--- assets/preload/stages/DCLStage.lua
+++ assets/preload/stages/DCLStage.lua
@@ -1,18 +1,18 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('DCLback', 'DCLback', -400, -300);
+	makeLuaSprite('DCLback', 'DCLback', -600, -250);
 	setScrollFactor('DCLback', 0.9, 0.9);
 	scaleObject('DCLback', 1, 1);
 
-	makeLuaSprite('DCLClouds', 'DCLClouds', -450, -100);
+	makeLuaSprite('DCLClouds', 'DCLClouds', -650, -50);
 	setScrollFactor('DCLClouds', 0.9, 0.9);
 	scaleObject('DCLClouds', 1, 1);
 
-	makeLuaSprite('DCLfront', 'DCLfront', -300, -60);
+	makeLuaSprite('DCLfront', 'DCLfront', -500, -10);
 	setScrollFactor('DCLfront', 0.9, 0.9);
 	scaleObject('DCLfront', 0.9, 0.9);
 
-	makeLuaSprite('DCLStage', 'DCLStage', -100, 110);
+	makeLuaSprite('DCLStage', 'DCLStage', -300, 170);
 	setScrollFactor('DCLStage', 0.9, 0.9);
 	scaleObject('DCLStage', 1, 1);
 
